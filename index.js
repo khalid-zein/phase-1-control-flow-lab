ecf263bbcf9ee20179eb83e40a6130f520ee38e1
khalid-zein <zeinkhalid004@gmail.com>
2022-11-26 22:19:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
   // Write your code here!
+
+  if (ride <= 400) {
+    return "This one is on me!";
+  } else if (ride > 2000 && ride <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (ride > 2500) {
+    return "No can do.";
+  } else {
+    return "No ride available";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+console.log(ternaryCheckCity("NYC"));
+
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
